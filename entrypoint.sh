9d2a1496ffc8fbe1244c9f429991706c87d51d37
Martin Kulich <martin.kulich@wandera.com>
2020-01-15 19:11:26 +0100

    Printing stats to stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,14 @@
 
 POOL6=${1:-"64:ff9b::/96"}
 
-finish() {
-  jool instance flush
-}
-trap finish SIGTERM SIGINT SIGQUIT
-
 jool instance flush
 jool instance add --iptables --pool6 ${POOL6}
 
-while true; do sleep 1000; done
+iptables -t mangle -A PREROUTING -j JOOL
+ip6tables -t mangle -A PREROUTING -j JOOL
+
+while true; do
+  echo "---"
+  jool stats display
+  sleep 10
+done
