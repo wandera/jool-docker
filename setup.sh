3a5ae224279a88cfccb157c09d3ee2569247407d
Peter Siman <peter.siman@wandera.com>
2020-08-03 18:16:17 +0200

    Set lowest IPv4 MTU

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 POOL6=${1:-"64:ff9b::/96"}
 
-jool instance add --iptables --pool6 ${POOL6} default
+jool instance add --iptables --pool6 ${POOL6} --lowest-ipv6-mtu 1440 default
 
 iptables -t mangle -A PREROUTING -j JOOL
 ip6tables -t mangle -A PREROUTING -j JOOL
