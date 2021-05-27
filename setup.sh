b09d36aad1f4a005bfacb1a2f93bd6f41a1f91e5
Laura Zelenku <laura.zelenku@wandera.com>
2021-05-27 15:18:08 +0200

    Add optional parameter to set lowest-ipv6-mtu

--- setup.sh
+++ setup.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
-POOL6=${1:-"64:ff9b::/96"}
+POOL6="64:ff9b::/96"
+LOWEST_IPV6_MTU=1280
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --pool6=*)
+      POOL6="${1#*=}"
+      ;;
+    --lowest-ipv6-mtu=*)
+      LOWEST_IPV6_MTU="${1#*=}"
+      ;;
+    *)
+      printf "************************************************************\n"
+      printf "* Error: Invalid argument '${1}'.\n"
+      printf "* Usage: setup.sh [--pool6=<IPv6>] [--lowest-ipv6-mtu=<mtu>]\n"
+      printf "************************************************************\n"
+      exit 1
+  esac
+  shift
+done
 
 jool instance add --iptables --pool6 ${POOL6} default
+jool global update lowest-ipv6-mtu ${LOWEST_IPV6_MTU}
 
 iptables -t mangle -A PREROUTING -j JOOL
 ip6tables -t mangle -A PREROUTING -j JOOL
