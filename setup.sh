ba6200081343219d2620a152b353967dde1c0b06
Jiri Frantisek <jiri.frantisek@wandera.com>
2021-06-17 12:59:11 +0200

    Add optional parameter to set handle-rst-during-fin-rcv

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 POOL6="64:ff9b::/96"
 LOWEST_IPV6_MTU=1280
+HANDLE_RST_DURING_FIN_RCV=false
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -11,10 +12,16 @@ while [ $# -gt 0 ]; do
     --lowest-ipv6-mtu=*)
       LOWEST_IPV6_MTU="${1#*=}"
       ;;
+    --handle-rst-during-fin-rcv=*)
+      HANDLE_RST_DURING_FIN_RCV="${1#*=}"
+      ;;
     *)
       printf "************************************************************\n"
       printf "* Error: Invalid argument '${1}'.\n"
-      printf "* Usage: setup.sh [--pool6=<IPv6>] [--lowest-ipv6-mtu=<mtu>]\n"
+      printf "* Usage: setup.sh [options...]\n"
+      printf "*        --pool6=<IPv6>\n"
+      printf "*        --lowest-ipv6-mtu=<mtu>\n"
+      printf "*        --handle-rst-during-fin-rcv=<boolean>\n"
       printf "************************************************************\n"
       exit 1
   esac
@@ -23,6 +30,7 @@ done
 
 jool instance add --iptables --pool6 ${POOL6} default
 jool global update lowest-ipv6-mtu ${LOWEST_IPV6_MTU}
+jool global update handle-rst-during-fin-rcv ${HANDLE_RST_DURING_FIN_RCV}
 
 iptables -t mangle -A PREROUTING -j JOOL
 ip6tables -t mangle -A PREROUTING -j JOOL
