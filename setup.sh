93d576981188c2cc77e628d24d95f16cb7fb42c9
Peter Siman <peter.siman@jamf.com>
2022-03-01 14:16:18 +0100

    WND-43445: Drop extrenally initiated TCP

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 POOL6="64:ff9b::/96"
 LOWEST_IPV6_MTU=1280
 HANDLE_RST_DURING_FIN_RCV="false"
+DROP_EXTERNALLY_INITIATED_TCP="false"
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -15,6 +16,9 @@ while [ $# -gt 0 ]; do
     --handle-rst-during-fin-rcv)
       HANDLE_RST_DURING_FIN_RCV="true"
       ;;
+    --drop-externally-initiated-tcp)
+      DROP_EXTERNALLY_INITIATED_TCP="true"
+      ;;
     *)
       printf "************************************************************\n"
       printf "* Error: Invalid argument '${1}'.\n"
@@ -22,6 +26,7 @@ while [ $# -gt 0 ]; do
       printf "*        --pool6=<IPv6>\n"
       printf "*        --lowest-ipv6-mtu=<mtu>\n"
       printf "*        --handle-rst-during-fin-rcv\n"
+      printf "*        --drop-externally-initiated-tcp\n"
       printf "************************************************************\n"
       exit 1
   esac
@@ -31,6 +36,7 @@ done
 jool instance add --iptables --pool6 ${POOL6} default
 jool global update lowest-ipv6-mtu ${LOWEST_IPV6_MTU}
 jool global update handle-rst-during-fin-rcv ${HANDLE_RST_DURING_FIN_RCV}
+jool global update drop-externally-initiated-tcp ${DROP_EXTERNALLY_INITIATED_TCP}
 
 iptables -t mangle -A PREROUTING -j JOOL
 ip6tables -t mangle -A PREROUTING -j JOOL
